20aeb63bfcbdafa7408f09e865fe980d6ab807ef
Umesh Namdev <67787316+umeshn95@users.noreply.github.com>
2022-01-04 11:28:23 +0000

    Test_module1

---
+++ build/precache-manifest.e463cf7148bf84780bf8ec07c0dbddc6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9312ded5df047aeb00c6f0a01233f603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68072e20885cfe8485d8",
+    "url": "/static/css/2.c3aa865c.chunk.css"
+  },
+  {
+    "revision": "f27daa84a9317efa2209",
+    "url": "/static/css/main.06a317df.chunk.css"
+  },
+  {
+    "revision": "68072e20885cfe8485d8",
+    "url": "/static/js/2.2f320db4.chunk.js"
+  },
+  {
+    "revision": "801da633c9827a53287225e1c5741172",
+    "url": "/static/js/2.2f320db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f27daa84a9317efa2209",
+    "url": "/static/js/main.04142604.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
